82ad2b5e4d2b255f36ba8f535b948a921371d438
theartdr <theartdr@gmail.com>
2023-05-28 23:39:19 +0900

    css

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const numForm = document.querySelector("#num-form");
 const max = document.querySelector("#max");
 const guessNum = document.querySelector("#guess-num");
-const button = document.querySelector("#play")
+const button = document.querySelector("#play");
 
 function displayResult(result, msg) {
   const resultOutput = document.querySelector("#num-form h2");
-  const mainOutput = document.querySelector("p");
+  const mainOutput = document.querySelector("#num-form p");
   resultOutput.innerText = result;
   mainOutput.innerText = msg;
 }
 
 function mouseClick() {
-  const maxValue = max.value;
-  const gValue = guessNum.value;
-  const rValue = Math.floor(Math.random() * maxValue);
+  const maxValue = parseInt(max.value);
+  const gValue = parseInt(guessNum.value);
+  const rValue = 1 + Math.floor(Math.random() * maxValue);
   let displayMsg =`Max = ${maxValue}, Guess = ${gValue}, Random = ${rValue}`;
-  choice.innerText = `You chose: ${gValue},the machine chose:${rValue}.`;
+  // choice.innerText = `You chose: ${gValue},the machine chose:${rValue}.`;
   if (gValue === rValue) {
     displayResult("You won!", displayMsg);
   } else {
@@ -23,4 +23,4 @@ function mouseClick() {
   }
 }
 
-button.addEventListener("submit", mouseClick);
+button.addEventListener("click", mouseClick);
